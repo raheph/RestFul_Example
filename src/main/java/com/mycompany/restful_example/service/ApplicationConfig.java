8817242b3c21b13f392667f9f3ffc174440da6c2
raheph <raheph@gmail.com>
2014-09-24 01:21:13 -0700

    an example for restfu webservice l, mySQL, and AnjularJs

---
+++ src/main/java/com/mycompany/restful_example/service/ApplicationConfig.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.restful_example.service;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author admin
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.mycompany.restful_example.NewCrossOriginResourceSharingFilter.class);
+        resources.add(com.mycompany.restful_example.service.CustomerFacadeREST.class);
+        resources.add(com.mycompany.restful_example.service.DiscountCodeFacadeREST.class);
+        resources.add(com.mycompany.restful_example.service.MicroMarketFacadeREST.class);
+    }
+    
+}
